52c70c961c5ba9a35132ea264f4e7a7f66f6fa85
Valdemar Ceccon <valdemar.ceccon@gmail.com>
2019-08-07 01:23:07 -0300

    adicionado javadoc com descrição do trabalho

--- src/main/java/trabalho1/Controle.java
+++ src/main/java/trabalho1/Controle.java
@@ -1,5 +1,15 @@
 package trabalho1;
 
+/**
+ * Classe Controle
+ *   a. Contém o método main com os seguintes passos:
+ *       i. Imprime a cor do semáforo
+ *       ii. Repete 10 vezes as seguintes operações:
+ *           1. Muda a cor atual do semáforo para a próxima cor
+ *           2. Imprime a cor do semáforo
+ *       iii. Fixa a cor do semáforo em amarelo
+ *       iv. Imprime a cor do semáforo
+ */
 public class Controle {
     public static void main(String[] args) {
 
